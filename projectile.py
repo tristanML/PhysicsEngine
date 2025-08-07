63f05a04aa4465d7cb32f30acda8ebb25ce85a52
Tristan  Laanait <tristanlaanait@Tristans-Laptop.attlocal.net>
2025-08-07 16:28:12 -0400

    Added support for continuous vector functions (via Sympy) in Projectile Class

--- projectile.py
+++ projectile.py
@@ -2,22 +2,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import math
+from sympy import symbols, sympify, diff
 g = 9.81
 step = 0.1
 theta = 45
+t = symbols("t")
+z_v = np.array([0,0,0])
 
 class Projectile:
-    def __init__(self, m, p, v, a, f):
-        self.m = m
+    def __init__(self, p, td, m, v = z_v, a = z_v, f = z_v):
+        
         self.p = p
-        self.p_r = [p]
-        self.v = v
-        self.v_r = [v]
-        self.a = a
-        self.a_r = [a]
-        self.f = f
-        self.nf = sum(f)
+        self.td = td
         self.time = 0
+        self.m = m
+        self.nf = sum(f)
+        
+        if not td:
+            self.v = v
+            self.a = a
+            self.f = f
+            self.p_r = [p]
+            self.v_r = [v]
+            self.a_r = [a]
+            
+        else:
+            self.v = np.array([diff(i) for i in self.p])
+            self.a = np.array([diff(i) for i in self.v])
+            self.f = self.a / self.m
+            self.p_r = [[float(sympify(i).subs(t, self.time)) for i in self.p]]
+            self.v_r = [[float(sympify(i).subs(t, self.time)) for i in self.v]]
+            self.a_r = [[float(sympify(i).subs(t, self.time)) for i in self.a]]
+
         print("Initial Position:", self.p)
         print("Initial Velocity:", self.v)
         print("Initial Acceleration:", self.a)
@@ -27,26 +43,33 @@ class Projectile:
         self.time = current_time
         return self.time
 
+    def delta_time(self, current_time):
+        return current_time - self.time
+
+    def position_calc(self, current_time):
+        if not self.td:
+            delta_time = self.delta_time(current_time)
+            self.p = self.p + self.v * delta_time + 0.5 * self.a * delta_time ** 2
+            self.velocity_calc(current_time)
+            self.p_r.append(self.p)
+            self.time_set(current_time)
+            return delta_time, self.p
+        p = [float(sympify(i).subs(t, current_time)) for i in self.p]
+        self.p_r.append(p)
+        return current_time, self.p
+    
+    def velocity_calc(self, current_time):
+        if not self.td:
+            delta_time = self.delta_time(current_time)
+            self.v = self.v + self.a * delta_time
+            self.v_r.append(self.v)
+            self.time_set(current_time)
+            return delta_time, self.v
+
     def apply_force(self):
         self.nf = sum(self.f)
         self.a = np.add(self.a, self.nf / self.m)
         return self.a
-
-    def delta_time(self, current_time):
-        return current_time - self.time
-
-    def velocity_calc(self, current_time):
-        delta_time = self.delta_time(current_time)
-        self.v = self.v + self.a * delta_time
-        self.v_r.append(self.v)
-        return delta_time, self.v
-
-    def position_calc(self, current_time):
-        delta_time = self.delta_time(current_time)
-        self.p = self.p + self.v * delta_time + 0.5 * self.a * delta_time ** 2
-        self.velocity_calc(current_time)
-        self.p_r.append(self.p)
-        return delta_time, self.p
 
 class System:
     def __init__(self, origin, dimensions, gravity, objects):
@@ -61,45 +84,44 @@ class System:
             print(object.apply_force())      
 
 position = np.array([0, 0, 0])
-velocity = np.array([0, 10, 0])
+velocity = np.array([0, 1, 0])
 acceleration = np.array([0, 0, 0])
-ball = Projectile(1, position, velocity, acceleration, [])
+discrete_ball = Projectile(position, False, 1, velocity, acceleration)
 
-origin = np.array([0, 0, 0])
-dim = np.array([10, 10, 10])
-grav = np.array([0, 0, -10])
-sys = System(origin, dim, grav, [ball])
+position = np.array([t, t, t])
+continuous_ball = Projectile(position, True, 1)
 
-t = 0
-ball.apply_force()
+# origin = np.array([0, 0, 0])
+# dim = np.array([10, 10, 10])
+# grav = np.array([0, 0, -10])
+# sys = System(origin, dim, grav, [ball])
 
-for n in range(100):
-    ball.position_calc(t)
-    ball.time_set(t)
-    t += step
+global_time = 0
 
+for n in range(10):
+    print(discrete_ball.position_calc(global_time))
+    continuous_ball.position_calc(global_time)
+    global_time += step
 
-# Generate some sample 3D data
+x1 = [i[0] for i in continuous_ball.p_r]
+y1 = [i[1] for i in continuous_ball.p_r]
+z1 = [i[2] for i in continuous_ball.p_r]
 
-num_points = 100
-x = [i[0] for i in ball.p_r]
-y = [i[1] for i in ball.p_r]
-z = [i[2] for i in ball.p_r]
+x2 = [i[0] for i in discrete_ball.p_r]
+y2 = [i[1] for i in discrete_ball.p_r]
+z2 = [i[2] for i in discrete_ball.p_r]
 
-# Create a figure and a 3D axes object
+print(x1)
+print(x2)
+
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-# Create the 3D scatter plot
-ax.scatter(x, y, z)
+ax.scatter(x1, y1, z1, ["red"])
+ax.scatter(x2, y2, z2, ["blue"])
 
-# Set axis labels
 ax.set_xlabel('X-axis')
 ax.set_ylabel('Y-axis')
 ax.set_zlabel('Z-axis')
 
-# Set a title for the plot
-ax.set_title('Basic 3D Scatter Plot')
-
-# Display the plot
 plt.show()
